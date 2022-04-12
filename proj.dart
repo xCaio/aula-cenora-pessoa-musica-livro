23c7761d70156c7577dcdf5192ce381bf892422d
Caio Santos <50676424+xCaio@users.noreply.github.com>
2022-04-12 19:47:35 -0300

    Add files via upload

---
+++ proj.dart
@@ -0,0 +1,37 @@
+import 'package:proj/mp3Player.dart';
+import 'package:proj/musica.dart';
+import 'package:proj/pessoa.dart';
+
+void main(List<String> arguments) {
+  Pessoa pessoa = Pessoa();
+  pessoa.nome = "Caio";
+  pessoa.idade = 20;
+  print("O nome é " + pessoa.nome);
+  print(pessoa.idade);
+  pessoa.adicionaAmigo(pessoa);
+  pessoa.nome = "Caio";
+  pessoa.briga(pessoa);
+
+  Mp3Player mp3player = Mp3Player();
+  print(mp3player.lista_de_musicas);
+
+  Musica musica = Musica("", "");
+  Musica musica2 = Musica("", "");
+  
+  //ADICIONANDO MUSICA
+  musica.nome ="Chicago";
+  musica.artista ="YungNobre";
+  mp3player.adicionaMusica(musica);
+
+  //ADICIONANDO OUTRA MUSICA
+  musica2.nome = "Olha pra mim";
+  musica2.artista = "Sueth";
+  mp3player.adicionaMusica(musica2);
+  print(mp3player.lista_de_musicas);
+
+  //REMOVENDO A PRIMEIRA MUSICA
+  mp3player.removeMusica(musica);
+  print(mp3player.lista_de_musicas);
+
+  //
+}
